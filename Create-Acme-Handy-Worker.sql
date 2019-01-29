871fff01b569effae32c33d9c7391d12c215a1d9
angel <ppen_llamas@hotmail.com>
2019-01-29 20:07:42 +0100

    ctualizacion

--- Create-Acme-Handy-Worker.sql
+++ Create-Acme-Handy-Worker.sql
@@ -1,17 +1,9 @@
 start transaction;
 
-create database `acme-handy-worker`;
-
-use `acme-handy-worker`;
-
-create user 'acme-user'@'%' identified by password '*4F10007AADA9EE3DBB2CC36575DFC6F4FDE27577';
-create user 'acme-manager'@'%' identified by password '*FDB8CD304EB2317D10C95D797A4BD7492560F55F';
 
 
+use `byvxbfpcurkfls1aonhn`;
 
-grant select, insert, update, delete	on `acme-handy-worker`.* to 'acme-user'@'%';
-
-grant select, insert, update, delete, create, drop, references, index, alter, create temporary tables, lock tables, create view, create routine, alter routine, execute, trigger, show view on `acme-handy-worker`.* to 'acme-manager'@'%';
 
 -- MySQL dump 10.13  Distrib 5.7.21, for Win64 (x86_64)
 --
